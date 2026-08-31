ab5a9b5893a366c1686b2d374a891cd17bc27310
Alan DuBose <badubose2016@outlook.com>
2026-08-30 19:10:48 -0500

    Completed Exercise 1,2 & 3

---
+++ A_Good_First_Program/ex3.py
@@ -0,0 +1,23 @@
+print("I will now count my chickens:")
+
+print("Hens", 25+30/6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+print(3+2+1-5+4%2-1/4+6)
+
+print("Is it true that 3 + 2 < 5 -7?")
+
+print(3+ 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+# in these last three lines, the output is out of context because you do not see the numbers. You only see the question and the answer. It needs better output clarification.
+print("Is it greater?", 5 > -2) # this should be "Is 5 greater than -2?"
+print("Is it greater or equal?", 5 >= -2) # this should be "Is 5 greater or equal to -2?"
+print("Is it less or equal?", 5 <= -2) # this should be "Is 5 less or equal to -2?"
